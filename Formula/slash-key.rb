e123e3a2fb2f14e9c9a7efdb0216a2e6804f4578
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-06-12 21:17:44 +0900

    Fix tap formula metadata

--- Formula/slash-key.rb
+++ Formula/slash-key.rb
@@ -3,7 +3,6 @@ class SlashKey < Formula
   homepage "https://github.com/kmatsushita1012/slash-key"
   url "https://github.com/kmatsushita1012/slash-key/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "6ca99647d7d4a63f5109b317e9ec8505d4e7f852bd4120845f4ea71d8e77d512"
-  license "MIT"
 
   depends_on "go" => :build
 
