adf65203910a2e0718a999a22430e37d00a6ec97
kmatsushita1012 <kmatsushita1012@icloud.com>
2026-07-23 00:37:17 +0900

    Fix Remote Sim cask download URL

--- Casks/remote-sim.rb
+++ Casks/remote-sim.rb
@@ -2,7 +2,7 @@ cask "remote-sim" do
   version "0.1.0"
   sha256 "18ea13b27cef5ab1ccb463d95a49ed14570eb84a21c3606602f200640c4f3c56"
 
-  url "https://github.com/kmatsushita1012/RemoteHub/releases/download/v#{version}/Remote%20Sim-#{version}.dmg"
+  url "https://github.com/kmatsushita1012/RemoteHub/releases/download/v#{version}/Remote.Sim-#{version}.dmg"
   name "Remote Sim"
   desc "PCとスマホを連携してシミュレータを確認・操作するアプリ"
   homepage "https://github.com/kmatsushita1012/RemoteHub"
