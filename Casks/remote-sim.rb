089d77d332422d4b621cd2de28e2c27e92c117b4
Matsu <55143274+kmatsushita1012@users.noreply.github.com>
2026-08-08 17:20:41 +0900

    Restore RemoteSim 2.0.0 cask

--- Casks/remote-sim.rb
+++ Casks/remote-sim.rb
@@ -1,11 +1,11 @@
-cask "remote-sim" do
-  version "2.1.0"
-  sha256 "30261ca61dd61fe5e920ddafd77ef5abfcc6d1197282f03a7a40acc8a3f7de14"
-
-  url "https://github.com/kmatsushita1012/RemoteSim/releases/download/v#{version}/RemoteSim-#{version}.dmg"
-  name "RemoteSim"
-  desc "View and control Mac simulators from a smartphone"
-  homepage "https://github.com/kmatsushita1012/RemoteSim"
-
-  app "RemoteSim.app"
+cask "'remote-sim'" do
+  version "'2.0.0'"
+  sha256 "'aac9b274d33e9bbb7cfb108e054863d2a3510db238ed3a7d3c29784368e3576f'"
+  
+  url "'https://github.com/kmatsushita1012/RemoteSim/releases/download/v#{version}/RemoteSim-#{version}.dmg'"
+  name "'RemoteSim'"
+  desc "'View and control Mac simulators from a smartphone'"
+  homepage "'https://github.com/kmatsushita1012/RemoteSim'"
+  
+  app "'RemoteSim.app'"
 end
